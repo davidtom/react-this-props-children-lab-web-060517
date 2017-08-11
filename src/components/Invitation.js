b348d78adc1125717a39fb2d149e8f330aa8d99f
davidtom <davidjtomczyk@gmail.com>
2017-08-11 16:47:54 -0400

    complete lab

--- src/components/Invitation.js
+++ src/components/Invitation.js
@@ -1,1 +1,20 @@
 // Code Invitation Component Here
+import React from "react"
+
+class Invitation extends React.Component {
+  constructor() {
+    super()
+
+  }
+  render(){
+    console.log(this.props.children)
+    return(
+      <div>
+      <h1>You've been invited!</h1>
+      <div>{this.props.children}</div>
+      </div>
+    )
+  }
+}
+
+export default Invitation
--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -1,1 +1,25 @@
-// Code ThemedDecoration Component Here
+// Code ThemedDecoration Component Hereimport React from "react"
+import React from "react"
+
+class ThemedDecoration extends React.Component {
+  constructor() {
+    super()
+
+  }
+  render(){
+
+    const childrenWithThemProp = React.Children.map(this.props.children, child => {
+      return (
+        <div className={this.props.theme}>{child}</div>
+      );
+    });
+
+    return(
+      <div>
+      {childrenWithThemProp}
+      </div>
+    )
+  }
+}
+
+export default ThemedDecoration
